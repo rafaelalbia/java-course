2238ac302bebb7a9b027c21f8f523c426677ac8c
rafaelalbia <rafaelalbia@outlook.com>
2021-01-21 22:11:58 -0300

    Example import

---
+++ lessons/example1.java
@@ -0,0 +1,15 @@
+package lessons;
+
+public class example1 {
+    public static void main(String[] Args) {
+        // Example 1
+        int x, y;
+        
+        x = 5;
+        
+        y = 2 * x;
+        
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
