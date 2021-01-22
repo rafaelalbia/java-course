e16f2351f5bf17f53d1fdd5cd864550ac0c6b507
rafaelalbia <rafaelalbia@outlook.com>
2021-01-21 22:57:05 -0300

    Example import

---
+++ lessons/example10.java
@@ -0,0 +1,22 @@
+package lessons;
+
+import java.util.Scanner;
+
+public class example10 {
+
+    public static void main(String[] Args) {
+        // Example 10
+        Scanner input = new Scanner(System.in);
+
+        String x;
+        int y;
+        double z;
+
+        x = input.next();
+        y = input.nextInt();
+        z = input.nextDouble();
+        input.close();
+
+        System.out.printf("Data: %s %d %.2f%n", x, y, z);
+    }
+}
