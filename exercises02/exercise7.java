00da5c7529c512773c387480bf1685ff2eb47d4f
rafaelalbia <rafaelalbia@outlook.com>
2021-01-24 18:40:25 -0300

    Exercise import

--- exercises02/exercise7.java
+++ exercises02/exercise7.java
@@ -1,8 +1,30 @@
 package exercises02;
 
+import java.util.Scanner;
+
 public class exercise7 {
     
     public static void main(String[] Args) {
-        
+
+        Scanner input = new Scanner(System.in);
+
+        double x, y;
+
+        System.out.print("Enter a value (x and y): ");
+        x = input.nextDouble();
+        y = input.nextDouble();
+
+        if (x > 0 && y > 0) {
+            System.out.println("Q1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Q2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Q3");
+        } else if (x > 0 && y < 0) {
+            System.out.println("Q4");
+        } else {
+            System.out.println("Origin");
+        }
+        input.close();
     }
 }
