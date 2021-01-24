19ea13c92e47998c8d9c1d9c629d7a58df2b1e9e
rafaelalbia <rafaelalbia@outlook.com>
2021-01-23 21:21:00 -0300

    Exercises import

--- exercises/exercise1.java
+++ exercises/exercise1.java
@@ -1,8 +1,24 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class exercise1 {
     
     public static void main(String[] Args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int firstValue, secondValue, resultSum;
+
+        System.out.print("First value: ");
+        firstValue = input.nextInt();
+        System.out.print("Second value: ");
+        secondValue = input.nextInt();
+        input.close();
+        resultSum = firstValue + secondValue;
+
+        System.out.println("The result of sum is " + resultSum);
+
     }
 
 }
--- exercises/exercise2.java
+++ exercises/exercise2.java
@@ -1,8 +1,16 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class exercise2 {
     
     public static void main(String[] Args){
+
+        Scanner input = new Scanner(System.in);
+
+        double radius;
+
+        
     }
 
 }
