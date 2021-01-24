91c58cb0e4fb02be78501e8442a0f5c668781d9c
rafaelalbia <rafaelalbia@outlook.com>
2021-01-23 22:47:50 -0300

    Exercises import

--- exercises/exercise5.java
+++ exercises/exercise5.java
@@ -1,8 +1,32 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class exercise5 {
 
     public static void main(String[] Args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int order, amount;
+        double unitaryValue, totalValue;
+
+        System.out.print("Order, amount and unitary value (first product): ");
+        order = input.nextInt();
+        amount = input.nextInt();
+        unitaryValue = input.nextDouble();
+
+        totalValue = amount * unitaryValue;
+
+        System.out.print("Order, amount and unitary value (second product): ");
+        order = input.nextInt();
+        amount = input.nextInt();
+        unitaryValue = input.nextDouble();
+        input.close();
+
+        totalValue += amount * unitaryValue;
+
+        System.out.printf("Total value: $ %.2f%n", totalValue);
     }
     
 }
