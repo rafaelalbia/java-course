9042e5287ac9c0d9f6db3ba90971be5eb9ec020d
rafaelalbia <rafaelalbia@outlook.com>
2021-01-24 19:59:32 -0300

    Exercise import

--- examples/example13.java
+++ examples/example13.java
@@ -1,9 +1,22 @@
 package examples;
 
+import java.util.Scanner;
+
 public class example13 {
     
     public static void main(String[] Args) {
 
+        Scanner input = new Scanner(System.in);
+
+        int minutes = input.nextInt();
+
+        double fee = 50.0;
+        if (minutes > 100) {
+            fee += (minutes - 100) * 2.0;
+        }
+
+        System.out.printf("Total value: $ %.2f%n", fee);
+        input.close();
     }
     
 }
