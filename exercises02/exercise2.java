4289c0065550d255982a8c1966b516b8879738cc
rafaelalbia <rafaelalbia@outlook.com>
2021-01-24 16:42:28 -0300

    Exercise import

--- exercises02/exercise2.java
+++ exercises02/exercise2.java
@@ -1,8 +1,22 @@
 package exercises02;
 
+import java.util.Scanner;
+
 public class exercise2 {
     
     public static void main(String[] Args) {
-        
+
+        Scanner input = new Scanner(System.in);
+
+        int number;
+
+        System.out.print("Enter a number: ");
+        number = input.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("This is even number");
+        } else {
+            System.out.println("This is odd number");
+        }
     }
 }
