4fb6f4d5c4d77941fa3e94c70942de1b867dbf36
rafaelalbia <rafaelalbia@outlook.com>
2021-01-23 21:38:42 -0300

    Exercises import

--- exercises/exercise3.java
+++ exercises/exercise3.java
@@ -1,8 +1,28 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class exercise3 {
 
     public static void main(String[] Args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int A, B, C, D, difference;
+
+        System.out.print("Value of A: ");
+        A = input.nextInt();
+        System.out.print("Value of B: ");
+        B = input.nextInt();
+        System.out.print("Value of C: ");
+        C = input.nextInt();
+        System.out.print("Value of D: ");
+        D = input.nextInt();
+        input.close();
+        difference = (A * B - C * D);
+
+        System.out.println("The difference is equal " + difference);
+
     }
 
 }
