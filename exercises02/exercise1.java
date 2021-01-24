06426d1479018eaac5355ba59b344e5464f17578
rafaelalbia <rafaelalbia@outlook.com>
2021-01-24 16:31:29 -0300

    Exercise import

--- exercises02/exercise1.java
+++ exercises02/exercise1.java
@@ -1,8 +1,23 @@
 package exercises02;
 
+import java.util.Scanner;
+
 public class exercise1 {
 
     public static void main(String[] Args) {
-        
+
+        Scanner input = new Scanner(System.in);
+
+        int number;
+
+        System.out.print("Enter a number: ");
+        number = input.nextInt();
+
+        if (number < 0) {
+            System.out.println("The number is NEGATIVE");
+        } else {
+            System.out.println("The number is not NEGATIVE");
+        }
+        input.close();
     }
 }
