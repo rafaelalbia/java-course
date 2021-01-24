37269d9932eb5e9d3b23dec215f7b7faede20e1d
rafaelalbia <rafaelalbia@outlook.com>
2021-01-24 15:52:40 -0300

    Example import

--- examples/example12.java
+++ examples/example12.java
@@ -1,17 +1,27 @@
 package examples;
 
+import java.util.Scanner;
+
 public class example12 {
     
     public static void main(String[] Args) {
 
-        int x = 6;
+        Scanner input = new Scanner(System.in);
+        int hour;
 
-        System.out.println("Bom dia");
-
-        if (x < 0) {
-            System.out.println("Boa tarde");
+        System.out.print("What time? ");
+        hour = input.nextInt();
+        
+        if (hour < 12) {
+            System.out.println("Good morning");
+        } else {
+            if (hour < 18) {
+                System.out.println("Good afternoon");
+            }
+            else {
+                System.out.println("Good night");
+            }
         }
-
-        System.out.println("Boa noite");
+        input.close();
     }
 }
