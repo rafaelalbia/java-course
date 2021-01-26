4a7979f8c9d591bd0cbf304d3ab4098eb0dee776
rafaelalbia <rafaelalbia@outlook.com>
2021-01-25 22:56:08 -0300

    Example import

---
+++ examples/example15.java
@@ -0,0 +1,16 @@
+package examples;
+
+import java.util.Scanner;
+
+public class example15 {
+
+    public static void main(String[] Args) {
+
+        Scanner input = new Scanner(System.in);
+        double price = 34.5;
+        double discount = (price < 20.0) ? price * 0.1 : price * 0.05;
+
+        System.out.println(discount);
+        input.close();
+    }
+}
