72899621d0fc48b9b5d05baa3d53fa3006dfe67f
rafaelalbia <rafaelalbia@outlook.com>
2021-05-27 23:10:37 -0300

    Example import

--- examples/example21.java
+++ examples/example21.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import jdk.javadoc.internal.tool.resources.javadoc;
-
 public class example21 {
 
     public static void main(String[] args) {
--- examples/example23.java
+++ examples/example23.java
@@ -3,7 +3,41 @@ import java.util.Scanner;
 public class example23 {
     
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter three numbers: ");
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        showResult(max(a, b, c));
+
+        // if (a > b && a > c) {
+        //     System.out.println("Higher = " + a);
+        // } else if (b > c) {
+        //     System.out.println("Higher = " + b);
+        // } else {
+        //     System.out.println("Higher = " + c);
+        // }
+
+        input.close();
+    }
+
+    public static int max(int x, int y, int z) {
+        int aux;
+
+        if (x > y && x > z) {
+            aux = x;
+        } else if (y > z) {
+            aux = y;
+        } else {
+            aux = z;
+        }
+
+        return aux;
+    }
+
+    public static void showResult(int value) {
+        System.out.println("Higher = " + value);
     }
 }
